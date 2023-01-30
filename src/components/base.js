7420d74f08d2803071b86e9c2a52194ac2cf8723
farhansayyed165 <farhansayyed1656@gmail.com>
2023-01-30 23:41:05 +0530

    starting out

---
+++ src/components/base.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Base = () => {
+    return (
+        <div className='base'>
+            <div className="container">
+                
+            </div>
+        </div>
+    );
+}
+
+export default Base;
