65d401712e07df424b50f887986f651d9a0673b6
farhansayyed165 <farhansayyed1656@gmail.com>
2023-02-01 23:33:59 +0530

    Got then search recommendations working

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,34 +1,39 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { AsyncPaginate } from 'react-select-async-paginate';
 import { GEO_API_URL, getGeoApiOption } from '../../api';
 
-const Search = ({onSearchChange}) => {
+const Search = ({ onSearchChange }) => {
 
     const [search, setSearch] = useState(null);
 
-    function handleOnChange(searchData){
+    function handleOnChange(searchData) {
         setSearch(searchData);
         onSearchChange(searchData);
 
     }
 
-    function loadOptions(inputValue) {
-    return fetch(`${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`, getGeoApiOption)
-	.then(response => response.json())
-	.then((response) => {
-        return{
-            options: response.data.map((city)=>{
-                return{
 
-                    value: `${city.lattitude} ${city.longitude} `,
-                    label: `${city.name}, ${city.countryCode}  `
+
+
+    function loadOptions(inputValue) {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`, getGeoApiOption)
+            .then(response => response.json())
+            .then((response) => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+
+                            value: `${city.lattitude} ${city.longitude} `,
+                            label: `${city.name}, ${city.countryCode}  `
+                        }
+                    })
+
                 }
             })
-
-        }
-    })
-	.catch(err => console.error(err));
+            .catch(err => console.error(err));
     }
+
+
     return (
         <div className='container'>
             <AsyncPaginate
