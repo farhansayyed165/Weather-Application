4f3aeb8b9af0c89744226a8e7705461ccdae7dea
farhansayyed165 <farhansayyed1656@gmail.com>
2023-02-02 23:53:20 +0530

    Updated current-weather.css

--- src/components/current_weather/current-weather.js
+++ src/components/current_weather/current-weather.js
@@ -3,8 +3,14 @@ import './current-weather.css'
 
 const CurrentWeather = () => {
     return (
-        <div>
-           Hello 
+        <div className='weather'>
+            <div className="top">
+                <div>
+                <p className="city">Pune</p>
+                <p className="weather-description">Sunny</p>
+                </div>
+            <img src="icons/01d.png" alt="weather icon" className='weather-icon' />
+            </div>
         </div>
     );
 }
