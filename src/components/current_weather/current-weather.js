f890aeee90dadfe623b4cb6a28b6def2cd4890e8
farhansayyed165 <farhansayyed1656@gmail.com>
2023-02-04 16:57:10 +0530

    replaced current weather styling

--- src/components/current_weather/current-weather.js
+++ src/components/current_weather/current-weather.js
@@ -4,34 +4,34 @@ import './current-weather.css'
 const CurrentWeather = ({data}) => {
     return (
         <div className='weather'>
-            <div className="top">
-                <div>
-                <p className="city">{data.city}</p>
-                <p className="weather-description">{data.weather[0].description}</p>
-                </div>
-            <img src={`icons/${data.weather[0].icon}.png`} alt="weather icon" className='weather-icon' />
+            <p className="city font-weight-600">{data.city}</p>
+        <div className="current-weather">
+        <img src={`icons/${data.weather[0].icon}.png`} alt="weather icon" className='weather-icon' />
+          <div className="desc-box">
+            <p className="temp">21°C</p>
+            <p className="desc font-weight-300">{data.weather[0].description}</p>
+          </div>
+          <div className="details">
+            <div className="d">
+              <div className="parameter-row">
+                <span className="parameter-label font-weight-300">Feels Like</span>
+                <span className="parameter-value font-weight-600">{data.main.feels_like}°C</span>
+              </div>
+              <div className="parameter-row">
+                <span className="parameter-label font-weight-300">Wind</span>
+                <span className="parameter-value font-weight-600">{data.wind.speed} m/s</span>
+              </div>
+              <div className="parameter-row">
+                <span className="parameter-label font-weight-300">Humidity</span>
+                <span className="parameter-value font-weight-600">{data.main.humidity}%</span>
+              </div>
+              <div className="parameter-row">
+                <span className="parameter-label font-weight-300">Pressure</span>
+                <span className="parameter-value font-weight-600">{data.main.pressure} hPa</span>
+              </div>
             </div>
-            <div className="bottom">
-                <p className="temperature">{Math.round(data.main.temp)}°C</p>
-                <div className="details">
-                    <div className="parameter-row">
-                        <span className="parameter-label">Feels Like</span>
-                        <span className="parameter-value">{data.main.feels_like}°C</span>
-                    </div>
-                    <div className="parameter-row">
-                        <span className="parameter-label">Wind</span>
-                        <span className="parameter-value">{data.wind.speed} m/s</span>
-                    </div>
-                    <div className="parameter-row">
-                        <span className="parameter-label">Humidity</span>
-                        <span className="parameter-value">{data.main.humidity}%</span>
-                    </div>
-                    <div className="parameter-row">
-                        <span className="parameter-label">Pressure</span>
-                        <span className="parameter-value">{data.main.pressure} hPa</span>
-                    </div>
-                </div>
-            </div>
+          </div>
+        </div>
         </div>
     );
 }
