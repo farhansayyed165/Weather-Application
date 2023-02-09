a357a962e0b4dffc7ffea50cba6ff0fec07fb93e
farhansayyed165 <farhansayyed1656@gmail.com>
2023-02-09 22:49:36 +0530

    added chart component(doesn't work)
    added the chart component but setChartData does not work :(

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Search from './components/search/search';
 import Forecast from './components/forecast/forecast';
 import CurrentWeather from './components/current_weather/current-weather';
+import LineChart from './components/chart/LineChart';
 import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
 import { useState } from 'react';
 
@@ -9,6 +10,7 @@ function App() {
 
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecastWeather, setforcastWeather] = useState(null);
+  const [chartData, setChartData] = useState(null);
 
   function handleOnSearchChange(searchData) {
     const [lat, lon] = searchData.value.split(" ");
@@ -23,19 +25,38 @@ function App() {
 
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
         setforcastWeather({ city: searchData.label, ...forecastResponse });
+        setChartData({
+          labels: forecastWeather.map((item, idx) => item.main.humidity),
+          datasets:[
+            {
+              label:"farhan",
+              data: forecastWeather.map((item, idx) => item.main.temp),
+              backgroundColor: "limegreen"
+            }
+          ],
+        })
+        
       })
       .catch((err) => console.log(err));
   }
 
+
   console.log(currentWeather);
   console.log(forecastWeather);
+  console.log("chart");
+  console.log(chartData);
 
 
   return (
+    <div>
+
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather}/>}
       {forecastWeather && <Forecast data={forecastWeather} currentData = {currentWeather}/>}
+    </div>
+      {chartData && <LineChart data={chartData}/>}
+
     </div>
   );
 }
---
+++ src/components/chart/LineChart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS } from 'chart.js'; 
+const LineChart = ({ data }) => {
+    return (
+        <Line data={data}/>
+    );
+}
+
+export default LineChart;
