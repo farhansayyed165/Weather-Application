518d08bf677b373ede725c55129d64305762de8e
farhansayyed165 <farhansayyed1656@gmail.com>
2023-02-09 20:07:47 +0530

    Added Finished img

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -13,7 +13,7 @@ const Forecast = ({ data, currentData }) => {
         <div className="future">
                 <div className="main-future">
                     <div className="day font-weight-600">{forecastDays[6]} (Today)</div>
-                    <img src={`icons/${currentData.weather[0].icon}.png`} alt="vv" />
+                    <img src={`icons/${currentData.weather[0].icon}.png`} alt="vv"  className="img-future"/>
                     <div className="text">
                         <p className='desc'>{currentData.weather[0].description}</p>
                         <p className="min-max-temp"><span className="max-temp font-weight-600">{Math.round(currentData.main.temp_max)}°C</span> <span className="min-temp">{Math.floor(currentData.main.feels_like)}°C</span></p>
@@ -48,7 +48,7 @@ const Forecast = ({ data, currentData }) => {
             <div className="future" key={idx}>
                 <div className="main-future">
                     <div className="day font-weight-600">{forecastDays[idx]}</div>
-                    <img src={`icons/${item.weather[0].icon}.png`} alt="vv" />
+                    <img src={`icons/${item.weather[0].icon}.png`} alt="vv" className="img-future"/>
                     <div className="text">
                         <p className='desc'>{item.weather[0].description}</p>
                         <p className="min-max-temp"><span className="max-temp font-weight-600">{Math.round(item.main.temp_max)}°C</span> <span className="min-temp">{Math.floor(item.main.feels_like)}°C</span></p>
